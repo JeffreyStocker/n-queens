ee210aa1542d1d1c99c346ccc1505783eff41fa3
jeffreystocker <kagesennin@gmail.com>
2017-10-18 20:06:43 -0700

    Placed skeleton and p-code for countNRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -31,15 +31,83 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var board = new Board({'n': n});
+  var boardMap = board.rows();
+  //helper function provides solution for position provided
+    // recieve back solution array for that position
   
+  //we will be iterating through all spots in the matrix
   
-  
+  for (var y = 0; y < boardMap.length; y++) {
+    for (var x = 0; x < boardMap[0].length; x++) {
+      board.togglePiece(y, x);
+      var potentialSolution = findRookSolutionAtPosition(x, y, board);
+    }
+  }
   
   
   var solutionCount = undefined; //fixme
-
+  //return the number of unique solutions
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
+};
+
+window.countNRooksSolutionsRecursive1 = function (n, prevBoard) {
+  if (!prevBoard) {
+    var board = new Board({'n': n});  
+  } else {
+    var board = prevBoard;
+  }
+  
+  var boardMap = board.rows();
+  var evaluatedSolutions = [];
+  //build board
+  //then set first piece
+    // recursive into the next iterations, where it finds the next valid peice
+    // recursion into the board to find the next piece
+    // pushed output into an output array
+  // then go to next piece 
+    //repete the recursion
+  //still going to have problems when we get past a certain point in the board
+  // maybe have a check for previes spot and if it finds a valid point at a previous point cancel the recusion as it would have found that already.
+  for (var y = 0; y < boardMap.length; y++) {
+    for (var x = 0; x < boardMap[0].length; x++) {
+      board.togglePiece(y, x);
+      
+      /// check for previous possible solution spots here
+      //if valid 
+      var tempSolution = innerfunction(board, x, y);
+      evaluatedSolutions.push(tempSolution);
+      //else
+      board.togglePiece(x, y);
+    }
+  }
+  
+  
+  var innerfunction = function (board, x, y) {
+    //also need to calculate the next spot in the grid
+    for (y; y < boardMap.length; y++) {
+      for (x; x < boardMap[0].length; x++) {
+        board.togglePiece(y, x);
+        //if hasAnyRooksConflicts is true
+        if (!board.hasAnyRooksConflicts()) {
+        //if false, keep the piece on
+        //reverse togglePiece
+          board.togglePiece(board, y, x);        
+        } else {
+          //recursive innerfunction
+        }
+      }
+    }  
+    return board;
+  };
+  
+};
+
+window.findRookSolutionAtPosition = function (y, x, currentBoard) {
+  
+  
+  // magicly return the solutions  
+  return solutionMatrix;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
