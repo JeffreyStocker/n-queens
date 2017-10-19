ba291339292f170ea1708593807d85fb7d55e0c8
jeffreystocker <kagesennin@gmail.com>
2017-10-18 17:32:01 -0700

    Implement findNRooksSolution, and pass tests

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,13 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({'n': n});
+
+  for (var i = 0; i < n; i++ ) {
+    board.togglePiece(i, i);
+  }
+  
+  var solution = board.rows();
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,6 +30,12 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
+  var board = new Board({'n': n});
+  
+  
+  
+  
+  
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
